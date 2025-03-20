bade75607e969523a42912012b4f0f652df66413
Frederik Mbius Rygaard <fmry@pop-os.localdomain>
2025-03-20 22:29:29 +0100

    Update

--- sample_scripts/football.py
+++ sample_scripts/football.py
@@ -16,8 +16,8 @@ def run_football(N:int=10, lam:float=1.0, max_iter:int=100, inter_method:str="li
     img1 = Image.open('sample_imgs/footballer1.png').resize((768, 768))
     img2 = Image.open('sample_imgs/footballer2.png').resize((768, 768))
     
-    prompt = 'a photo of a football player'
-    n_prompt='text, football player soccer sports'
+    prompt = 'a photo of a soccer player'
+    n_prompt='text, soccer sports player'
     CM.interpolate_new(img1, img2,  prompt=prompt, n_prompt=n_prompt, ddim_steps=200,  guide_scale=10,  out_dir='sample_results/football')
     
     return
