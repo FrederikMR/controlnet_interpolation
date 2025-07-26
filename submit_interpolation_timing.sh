a2f606f29f3caf5a1328dcea3e27ea297316dcb0
Frederik-DTU <s164222@student.dtu.dk>
2025-07-26 23:17:09 +0200

    Update

---
+++ submit_interpolation_timing.sh
@@ -0,0 +1,29 @@
+    #! /bin/bash
+    #BSUB -q gpuv100
+    #BSUB -J cat_Noise
+    #BSUB -n 4
+    #BSUB -gpu "num=1:mode=exclusive_process"
+    #BSUB -W 24:00
+    #BSUB -R "span[hosts=1]"
+    #BSUB -R "rusage[mem=16GB]"
+    #BSUB -u fmry@dtu.dk
+    #BSUB -B
+    #BSUB -N
+    #BSUB -o sendmeemail/error_%J.out 
+    #BSUB -e sendmeemail/output_%J.err 
+    
+    module swap python3/3.10.12
+    module swap cuda/12.0
+    module swap cudnn/v8.9.1.23-prod-cuda-12.X
+    
+    python3 run_interpolation_timing.py \
+        --model cat \
+        --method Noise \
+        --lam 1.0 \
+        --clip 0 \
+        --mu -1.0 \
+        --nu -1.0 \
+        --N 10 \
+        --max_iter 100 \
+        --ckpt_path /work3/fmry/models/controlnet/control_v11p_sd21_openpose.ckpt \
+    
