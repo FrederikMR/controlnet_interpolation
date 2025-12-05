1ef796bbd10c06225207aeaf0fffe71dfaef3cfe
Frederik Mbius Rygaard <fmry@pop-os.localdomain>
2025-12-05 23:35:17 +0100

    Update

--- cm.py
+++ cm.py
@@ -423,14 +423,11 @@ class ContextManager:
             data_curve = self.PGEORCE_Score_Data(left_image, right_image)
             
             
-            noisy_curve = ldm.sqrt_alphas_cumprod[t] * data_curve + ldm.sqrt_one_minus_alphas_cumprod[t] * noise
-            print(noisy_curve.shape)
+            #noisy_curve = ldm.sqrt_alphas_cumprod[t] * data_curve + ldm.sqrt_one_minus_alphas_cumprod[t] * noise
             noisy_curve = [self.ddim_sampler.encode(data_img, cond, cur_step, 
                                                     use_original_steps=False, return_intermediates=None,
                                                     unconditional_guidance_scale=1, unconditional_conditioning=un_cond)[0] for data_img in data_curve]
-            noisy_curve = torch.concatenate(noisy_curve, axis=0)
-            print(noisy_curve.shape)
-            
+            noisy_curve = torch.concatenate(noisy_curve, axis=0).reshape(-1,1,4,96,96)
             
             #noisy_curve = None
         elif self.inter_method == "ProbGEORCE_Score_Noise":
