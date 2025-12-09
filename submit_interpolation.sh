9cc329c113ad6715e8c924a371ac3d468b91e34d
Frederik-DTU <s164222@student.dtu.dk>
2025-12-09 09:05:57 +0100

    Update

--- submit_interpolation.sh
+++ submit_interpolation.sh
@@ -1,6 +1,6 @@
     #! /bin/bash
     #BSUB -q gpuv100
-    #BSUB -J cat_ProbGEORCE_Score_Data
+    #BSUB -J house_ProbGEORCE_Data
     #BSUB -n 4
     #BSUB -gpu "num=1:mode=exclusive_process"
     #BSUB -W 24:00
@@ -17,13 +17,17 @@
     module swap cudnn/v8.9.1.23-prod-cuda-12.X
     
     python3 run_interpolation.py \
-        --model cat \
-        --method ProbGEORCE_Score_Data \
+        --img_types house \
+        --computation_method ivp \
+        --n_images 2 \
+        --image_size 768 \
+        --target_prompt 1 \
+        --method ProbGEORCE_Data \
         --lam 10.0 \
         --clip 0 \
         --mu -1.0 \
         --nu -1.0 \
-        --N 10 \
+        --N 100 \
         --max_iter 100 \
         --ckpt_path /work3/fmry/models/controlnet/control_v11p_sd21_openpose.ckpt \
     
