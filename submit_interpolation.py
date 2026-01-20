4110aecd2938f9572a1f2b230c12dc4466d7ad47
Frederik Mbius Rygaard <fmry@pop-os.localdomain>
2026-01-20 11:28:10 +0100

    Update

--- submit_interpolation.py
+++ submit_interpolation.py
@@ -97,7 +97,7 @@ def loop_jobs(wait_time = 1.0):
     
     #model = ['house', 'mountain', 'aircraft', "lion_tiger"]
     model = ['cat']
-    computation_methods = ['bvp'] #['ivp', 'bvp']
+    computation_methods = ['ivp'] #['ivp', 'bvp']
     run_model(computation_methods, model, method, clip, lam, N, reg_types, interpolation_space, max_iter, wait_time)
     
     reg_types = ['score', 'score_naive']
