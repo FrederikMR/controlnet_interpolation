b6c1ea72adf3a1eee5115485a4e5a6b463aeccde
Frederik Mbius Rygaard <fmry@pop-os.localdomain>
2026-01-20 16:42:05 +0100

    Update

--- submit_interpolation.py
+++ submit_interpolation.py
@@ -105,7 +105,7 @@ def loop_jobs(wait_time = 1.0):
     reg_types = ['score']
     interpolation_space=['data', 'noise']
     lam =[1.0]
-    #run_model(computation_methods, model, method, clip, lam, N, reg_types, interpolation_space, max_iter, wait_time)
+    run_model(computation_methods, model, method, clip, lam, N, reg_types, interpolation_space, max_iter, wait_time)
     
     reg_types = ['score']#, 'score_naive']
     interpolation_space=['noise']
