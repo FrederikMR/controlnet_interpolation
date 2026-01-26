a8ccd15baf2c86150d99fa3d0d72e572267b3703
Frederik Mbius Rygaard <fmry@pop-os.localdomain>
2026-01-26 20:34:02 +0100

    UPdate

--- cm.py
+++ cm.py
@@ -1008,7 +1008,7 @@ class ContextManager:
             max_steps = int(ddim_steps * max_steps)
         base_dir, out_dir = self.create_out_dir(out_dir, "mean")
         
-        print(type(img[0]))
+        print(type(imgs[0]))
         
         imgs = self.images_to_tensors_raw(imgs, base_dir, "cuda")
         
